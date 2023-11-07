49ba0b5bbaf1041e9e5460c447df930fb693f803
fernando-r-costa <frc_vet@hotmail.com>
2023-11-07 12:33:47 -0300

    implementa: calculos e cores

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,119 @@
-import { useState } from "react";
 import { investments, reports } from "../data/data";
 
 export default function Main() {
-  
   let reportsSort = [];
 
   reportsSort = reports.sort((a, b) => a.month - b.month);
 
-  let teste = investments.map((investment) => (reportsSort.filter((report) => report.investmentId.includes(investment.id))))
+  function getShortMonthName(monthNumber) {
+    const months = [
+      "jan",
+      "fev",
+      "mar",
+      "abr",
+      "mai",
+      "jun",
+      "jul",
+      "ago",
+      "set",
+      "out",
+      "nov",
+      "dez",
+    ];
+    return months[monthNumber - 1] || "";
+  }
 
+  function processInvestmentData(investments, reportsSort) {
+    const processedData = [];
+
+    investments.forEach((investment) => {
+      const investmentData = {
+        description: investment.description,
+        reports: [],
+        totalValueChange: 0,
+      };
+
+      const investmentReports = reportsSort.filter((report) =>
+        report.investmentId.includes(investment.id)
+      );
+
+      investmentReports.forEach((report, index) => {
+        const currentReport = {
+          month: getShortMonthName(report.month), // Obtém o nome abreviado do mês
+          year: report.year,
+          value: report.value,
+          percentChange: 0,
+        };
+
+        if (index > 0) {
+          const previousReport = investmentReports[index - 1];
+          currentReport.percentChange =
+            ((currentReport.value - previousReport.value) /
+              previousReport.value) *
+            100;
+        }
+
+        investmentData.reports.push(currentReport);
+
+        if (index === 0) {
+          investmentData.totalValueChange = currentReport.value;
+        } else if (index === investmentReports.length - 1) {
+          investmentData.totalValueChange =
+            currentReport.value - investmentData.reports[0].value;
+        }
+      });
+
+      processedData.push(investmentData);
+    });
+
+    return processedData;
+  }
+
+  const processedData = processInvestmentData(investments, reportsSort);
+  console.log("🚀 ~ file: Main.jsx:47 ~ Main ~ processedData:", processedData);
 
   return (
     <main className="container mx-auto mt-4">
-      {investments.map((investment) => (
+      {processedData.map((investment) => (
         <div key={investment.id} className="my-2 border-2 rounded-sm">
-          <h2 className="text-center font-semibold p-1">
+          <h2 className="text-center font-bold text-xl p-1">
             {investment.description}
           </h2>
-          <h3 className="text-center font-semibold text-sm p-1">
-            Rendimento total: {`R$ ${614.54}`}
+          <h3 className="text-center font-bold text-lg p-1">
+            Rendimento total:
+            <span className={`${(investment.totalValueChange >= 0) ? 'text-green-600' : 'text-red-600'}`}>
+              {` R$ ${investment.totalValueChange.toLocaleString("pt-BR", {
+                maximumFractionDigits: 2,
+              })}
+            (${((investment.totalValueChange / 1000) * 100).toLocaleString(
+              "pt-BR",
+              { maximumFractionDigits: 2 }
+            )} %)`}
+            </span>
           </h3>
           <table className="table-auto w-full my-2 border-b-2">
             <tbody>
-              {reportsSort
-                .filter((report) => report.investmentId.includes(investment.id))
-                .map((report) => (
-                  <tr key={report.month}>
-                    <td>{`${report.month}/${report.year}`}</td>
-                    <td>{`${report.value.toLocaleString("pt-BR", {
+              {investment.reports.map((report, index) => (
+                <tr key={index}>
+                  <td className="pl-5 font-semibold">{`${report.month.toLocaleString(
+                    "pt-BR",
+                    { month: "short" }
+                  )}/${report.year}`}</td>
+                  <td className={`font-semibold pr-5 ${(report.percentChange >= 0) ? 'text-green-600' : 'text-red-600'}`}>
+                    {`${report.value.toLocaleString("pt-BR", {
                       minimumFractionDigits: 2,
                       style: "currency",
                       currency: "BRL",
                     })}`}
-                    </td>
-                    <td>{`${report.income} %`}</td>
-                  </tr>
-                ))}
+                  </td>
+                  <td className={`text-right font-semibold pr-5 ${(report.percentChange >= 0) ? 'text-green-600' : 'text-red-600'}`}>{`${report.percentChange.toLocaleString(
+                    "pt-BR",
+                    {
+                      minimumFractionDigits: 2,
+                    }
+                  )} %`}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
